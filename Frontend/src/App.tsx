ccf993b08576ee095d027d08c932f2abf3e1fe2b
Danielson <danielson7632@gmail.com>
2025-01-16 11:03:06 +0700

    setup frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+      <Button> Helo</Button>
+    </>
+  )
+}
+
+export default App
